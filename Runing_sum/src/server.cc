8f19a2fbbeba4903e6b32eb4c8c513b4e4be17b0
ALEYI <aleelaleyi@gmail.com>
2024-08-29 21:08:19 +0000

    Se avanzo el microservicio de runing sum

---
+++ Runing_sum/src/server.cc
@@ -0,0 +1,81 @@
+#include <grpcpp/grpcpp.h>
+#include "proto/signal_runing_sum.grpc.pb.h"
+#include "Eigen/Dense"
+#include <complex>
+#include <iostream>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using signal::Complex;
+using signal::RuningSumRequest;
+using signal::RuningSumResponse;
+using signal::SignalService;
+
+class SignalServiceImpl final : public SignalService::Service
+{
+public:
+    Status ComputeRuningSum(ServerContext* context, const RuningSumRequest* request,
+                            RuningSumResponse* reply) override
+    {
+        try
+        {
+            // Convertir la señal de entrada a Eigen::VectorXcd
+            Eigen::VectorXcd signal(request->signal_size());
+            for (int i = 0; i < request->signal_size(); i++)
+            {
+                const Complex& complex_msg = request->signal(i);
+                signal[i] = std::complex<double>(complex_msg.real(), complex_msg.imag());
+            }
+
+            // Calcular el RuningSum
+            Eigen::VectorXcd result = RuningSum(signal);
+
+            // Convertir el resultado a RuningSumResponse
+            for (int i = 0; i < result.size(); i++)
+            {
+                auto complex_number = result[i];
+                Complex* complex_message = reply->add_result();
+                complex_message->set_real(complex_number.real());
+                complex_message->set_imag(complex_number.imag());
+            }
+            return Status::OK;
+        }
+        catch (const std::exception& ex)
+        {
+            return Status(grpc::INVALID_ARGUMENT, ex.what());
+        }
+    }
+
+private:
+    Eigen::VectorXcd RuningSum(const Eigen::VectorXcd& Input)
+    {
+        int Size = Input.size();
+        Eigen::VectorXcd Output(Size);
+
+        if (Size <= 0) return Output;
+
+        Output[0] = Input[0];
+        for (size_t i = 1; i < Size; ++i) {
+            Output[i] = Output[i - 1] + Input[i];
+        }
+        return Output;
+    }
+};
+
+int main()
+{
+    std::string server_address("0.0.0.0:5001");
+    SignalServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
+    return 0;
+}
